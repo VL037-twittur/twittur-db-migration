b1fa195fb60aca57dda5189142d689719e0c1f9a
Vincent Low <vincent.low@gdn-commerce.com>
2023-05-31 20:20:06 +0700

    add Email table

---
+++ src/main/resources/db/migration/V4__create_email_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE email (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    recipient VARCHAR(255) NOT NULL,
+    subject VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    is_sent BOOLEAN NOT NULL,
+    created_by VARCHAR(255) NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    updated_by VARCHAR(255) NOT NULL,
+    updated_date TIMESTAMP NOT NULL,
+    mark_for_delete BOOLEAN NOT NULL
+)
