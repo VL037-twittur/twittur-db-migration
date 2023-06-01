d3d47002f94b11b5cf81e798aa1557c988bb8b29
Vincent Low <vincent.low@gdn-commerce.com>
2023-06-01 08:37:13 +0700

    reconsider email template table

--- src/main/resources/db/migration/V4__create_email_table.sql
+++ src/main/resources/db/migration/V4__create_email_table.sql
@@ -10,3 +10,14 @@ CREATE TABLE email (
     updated_date TIMESTAMP NOT NULL,
     mark_for_delete BOOLEAN NOT NULL
 );
+
+CREATE TABLE email_template (
+    id VARCHAR(36) NOT NULL PRIMARY KEY,
+    template_code VARCHAR(255) NOT NULL,
+    body TEXT NOT NULL,
+    created_by VARCHAR(255) NOT NULL,
+    created_date TIMESTAMP NOT NULL,
+    updated_by VARCHAR(255) NOT NULL,
+    updated_date TIMESTAMP NOT NULL,
+    mark_for_delete BOOLEAN NOT NULL
+);
